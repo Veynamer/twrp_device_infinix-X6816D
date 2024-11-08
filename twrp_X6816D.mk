9b1807be7168c8d83876b727a0032ce05f8a610b
Veynamer <panferovvladimir62@gmail.com>
2024-11-08 09:00:37 +0300

    fix building

--- twrp_X6816D.mk
+++ twrp_X6816D.mk
@@ -7,7 +7,7 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
@@ -19,4 +19,9 @@ PRODUCT_DEVICE := X6816D
 PRODUCT_NAME := twrp_X6816D
 PRODUCT_BRAND := Infinix
 PRODUCT_MODEL := Infinix X6816D
-PRODUCT_MANUFACTURER := infinix
+PRODUCT_MANUFACTURER := INFINIX MOBILITY LIMITED
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="X6816D-user 12 SP1A.210812.016 235 release-keys"
+
+BUILD_FINGERPRINT := Infinix/X6816D-RU/Infinix-X6816D:12/SP1A.210812.016/240407V163:user/release-keys
