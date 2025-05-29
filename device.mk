8ca74497aa1c2c7109f2632d7a2f743eab77d06a
Veynamer <panferovvladimir62@gmail.com>
2025-05-29 08:01:33 +0300

    fix compile

--- device.mk
+++ device.mk
@@ -20,6 +20,8 @@ AB_OTA_POSTINSTALL_CONFIG += \
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+TW_LOAD_VENDOR_BOOT_MODULES := true
+
 # VNDK
 PRODUCT_SHIPPING_API_LEVEL := 30
 PRODUCT_TARGET_VNDK_VERSION := 31
@@ -36,10 +38,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
     android.hardware.health@2.1-service \
-    libhealthd.$(PRODUCT_PLATFORM)
-#TW_LOAD_VENDOR_MODULES := "goodix_fp.ko  modules.load modules.load.recovery msm_drm.ko sprd_audcp_boot.ko  fpsensor_fp.ko sprd_sensor.ko aw32257_charger.ko charger-manager.ko tran_charger.ko musb_hdrc.ko musb_sprd.ko"
-TW_LOAD_VENDOR_MODULES := $(shell echo \"$(shell ls $(DEVICE_PATH)/recovery/root/lib/modules)\")
- 
+    libhealthd.$(PRODUCT_PLATFORM) 
+    
 # Boot control HAL
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.2-impl \
--- recovery/root/system/bin/load_modules.sh
+++ recovery/root/system/bin/load_modules.sh
@@ -6,8 +6,12 @@ load_panel()
 	insmod /lib/modules/gslX680.ko
 	insmod /lib/modules/adaptive-ts.ko
 	insmod /lib/modules/gpio.ko
+	insmod /lib/modules/gpio-sprd.ko
 	insmod /lib/modules/focaltech_ft8756_spi_ts.ko
-	insmod /lib/modules/sc27xx-vibra.ko
+	insmod /lib/modules/nvt_nt36xxx_spi_ts.ko
+	insmod /lib/modules/fortsense_fp.ko
+	insmod /lib/modules/sc27xx_tsensor_thermal.ko
+	insmod /lib/modules/tran_gesture.ko
 }
 
 
